6ffbb73ccd71090d3766fbd5e86ef32175e282b9
rajat <rajatpimpre7@gmail.com>
2026-02-06 16:04:03 +0530

    1st Commit
    (cherry picked from commit 325a85fd58768968f5acce8de6bdc2de67575545)

---
+++ productlist/productlist/src/main/java/com/bitsnbytes/product/entity/Product.java
@@ -0,0 +1,71 @@
+package com.bitsnbytes.product.entity;
+
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name="category_id", nullable = false)
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Product(Long id, String name, String description, Double price, Category category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Product() {
+    }
+}
---
+++ productlist/productlist/src/main/java/com/bitsnbytes/product/service/CategoryService.java
@@ -0,0 +1,28 @@
+package com.bitsnbytes.product.service;
+
+import com.bitsnbytes.product.dto.CategoryDTO;
+import com.bitsnbytes.product.entity.Category;
+import com.bitsnbytes.product.mapper.CategoryMapper;
+import com.bitsnbytes.product.repository.CategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    // create category
+    public CategoryDTO createCategory(CategoryDTO categoryDTO){
+        Category category = CategoryMapper.toCategoryEntity(categoryDTO);
+        category = categoryRepository.save(category);
+        return CategoryMapper.toCategoryDTO(category);
+
+    }
+    // get all categories
+    // get category by id
+    // delete category
+}
